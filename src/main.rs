08bfa7e065c969bf8f4e117a75cefd8c65f0b27a
pixup1 <pixup1@yahoo.com>
2024-08-27 00:01:01 +0200

    Added working display for small words

---
+++ src/main.rs
@@ -0,0 +1,142 @@
+// std
+use std::env;
+use std::io::stdout;
+//use std::io::Write;
+use std::time::Instant;
+
+// crates
+use getopts::Options;
+use crossterm;
+use rusttype::{point, Font, Scale};
+
+fn render_word(word: &str, font: &Font, height: f32, mapping_chars: &str) -> Vec<Vec<u8>> { // See https://github.com/redox-os/rusttype/blob/master/dev/examples/ascii.rs
+	// Compensate for the aspect ratio of monospace characters
+	let scale = Scale {
+		x: height * 2.0,
+		y: height,
+	};
+	
+	let height = height.ceil() as usize;
+	
+	let v_metrics = font.v_metrics(scale);
+	let offset = point(0.0, v_metrics.ascent);
+	let glyphs: Vec<_> = font.layout(word, scale, offset).collect();
+	
+	// Find the most visually pleasing width to display, imma be real idk what this does
+	let width = glyphs
+		.iter()
+		.rev()
+		.map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+		.next()
+		.unwrap_or(0.0)
+		.ceil() as usize;
+	
+	let mut pixels = vec![vec![b' '; width]; height]; // 2D array of characters, each row is a line of text
+	let mapping = mapping_chars.as_bytes();
+	let mapping_scale = (mapping.len() - 1) as f32;
+	
+	for g in glyphs {
+		if let Some(bb) = g.pixel_bounding_box() {
+			g.draw(|x, y, v| {
+				// v should be in the range 0.0 to 1.0
+				let i = (v * mapping_scale + 0.5) as usize;
+				let c = mapping.get(i).cloned().unwrap_or(mapping[mapping.len() - 1]); // If there's an error we just use the maximum value
+				let x = x as i32 + bb.min.x;
+				let y = y as i32 + bb.min.y;
+				// There's still a possibility that the glyph clips the boundaries of the bitmap
+				if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
+					let x = x as usize;
+					let y = y as usize;
+					pixels[y][x] = c;
+				}
+			})
+		}
+	}
+	
+	return pixels;
+}
+
+fn display(pixels: Vec<Vec<u8>>, x: u16, y: u16) { // Display pixels centered at x,y
+	let psize = (pixels[0].len() as u16, pixels.len() as u16);
+	let origin = (x - psize.0 / 2, y - psize.1 / 2);    
+	
+	let mut stdout = ::std::io::stdout();
+	
+	for i in 0..pixels.len() {
+		crossterm::execute!(stdout, crossterm::cursor::MoveTo(origin.0, origin.1 + i as u16)).unwrap();
+		for j in 0..pixels[i].len() {
+			print!("{}", pixels[i][j] as char);
+		}
+	}
+}
+
+fn print_usage(program: &str, opts: Options) {
+	let brief = format!("Usage: {} TEXT [options]", program);
+	print!("{}", opts.usage(&brief));
+}
+
+
+fn main() {
+	// getopts things
+	let args: Vec<String> = env::args().collect();
+	let program = args[0].clone();
+	
+	let mut opts = Options::new();
+	opts.optopt("f", "font", "set big font", "PATH");
+	opts.optflag("l", "loop", "loop text");
+	opts.optflag("h", "help", "print this help menu");
+	let matches = match opts.parse(&args[1..]) {
+		Ok(m) => { m }
+		Err(f) => { panic!("{}", f.to_string()) }
+	};
+
+	if matches.opt_present("h") {
+		print_usage(&program, opts);
+		return;
+	}
+	let do_loop = matches.opt_present("l");
+	let font = match matches.opt_str("f") {
+		Some(path) => Font::try_from_vec(std::fs::read(&path).expect("Error reading font")).expect("Error reading font"),
+		None => Font::try_from_bytes(include_bytes!("../fonts/Montserrat/Montserrat-Black.ttf")as &[u8]).expect("Error reading default font")
+	};
+	let text = if !matches.free.is_empty() {
+		matches.free[0].clone()
+	} else {
+		print_usage(&program, opts);
+		return;
+	};
+	
+	let mut stdout = stdout();
+	
+	crossterm::terminal::enable_raw_mode().unwrap();
+	crossterm::execute!(stdout, crossterm::cursor::Hide).unwrap();
+	crossterm::execute!(stdout, crossterm::terminal::DisableLineWrap).unwrap();
+	crossterm::execute!(stdout, crossterm::terminal::EnterAlternateScreen).unwrap();
+	
+	loop {
+		for word in text.split_whitespace() { // Main loop
+			let time: i32 =  300 + 60 * word.len() as i32; // Time to show current word for in milliseconds
+			let timer = Instant::now();
+			while timer.elapsed().as_millis() < time as u128 {
+				let progress = timer.elapsed().as_millis() as f32 / time as f32;
+				let tsize = crossterm::terminal::size().unwrap();
+				
+				let render = render_word(word, &font, (tsize.1 as f32)/3.0, " .:-=+*#%@"); 
+				//crossterm::execute!(stdout, crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
+				
+				if render[0].len() as u16 > tsize.0 {
+						
+				} else {
+					display(render, tsize.0 / 2, tsize.1 / 2);
+				}
+			}
+		}
+		
+		if !do_loop {
+			break;
+		}
+	}
+	
+	crossterm::execute!(stdout, crossterm::terminal::LeaveAlternateScreen).unwrap();
+	crossterm::execute!(stdout, crossterm::cursor::Show).unwrap();
+}
