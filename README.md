ff3dc1745d8ad00c1e0328988006aef508c54791
pixup1 <pixup1@yahoo.com>
2024-08-27 03:53:55 +0200

    Added basic functionnality + font license

---
+++ README.md
@@ -0,0 +1,13 @@
+# **big**
+
+Build with `cargo build --release`
+
+Usage: `target/release/big TEXT [options]`
+
+Options:
+```
+    -f, --font PATH     set font
+    -s, --speed INTEGER set text speed (default: 10)
+    -l, --loop          loop text
+    -h, --help          print this help menu
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // std
 use std::env;
 use std::io::stdout;
-//use std::io::Write;
 use std::time::Instant;
 
 // crates
 use getopts::Options;
 use crossterm;
 use rusttype::{point, Font, Scale};
+
+const PUNCTUATION: &str = ".,!?;:";
 
 fn render_word(word: &str, font: &Font, height: f32, mapping_chars: &str) -> Vec<Vec<u8>> { // See https://github.com/redox-os/rusttype/blob/master/dev/examples/ascii.rs
 	// Compensate for the aspect ratio of monospace characters
@@ -56,18 +57,25 @@ fn render_word(word: &str, font: &Font, height: f32, mapping_chars: &str) -> Vec
 	return pixels;
 }
 
-fn display(pixels: Vec<Vec<u8>>, x: u16, y: u16) { // Display pixels centered at x,y
-	let psize = (pixels[0].len() as u16, pixels.len() as u16);
-	let origin = (x - psize.0 / 2, y - psize.1 / 2);    
+fn display(pixels: &mut Vec<Vec<u8>>, todraw: Vec<Vec<u8>>, x: i32, y: i32) { // Display pixels centered at x,y
+	let origin = (x - todraw[0].len() as i32 / 2, y - todraw.len() as i32 / 2);    
 	
-	let mut stdout = ::std::io::stdout();
-	
-	for i in 0..pixels.len() {
-		crossterm::execute!(stdout, crossterm::cursor::MoveTo(origin.0, origin.1 + i as u16)).unwrap();
-		for j in 0..pixels[i].len() {
-			print!("{}", pixels[i][j] as char);
+	for i in 0..todraw.len() {
+		for j in 0..todraw[i].len() {
+			let x = origin.0 + j as i32;
+			let y = origin.1 + i as i32;
+			if x >= 0 && x < pixels[0].len() as i32 && y >= 0 && y < pixels.len() as i32 {
+				pixels[y as usize][x as usize] = todraw[i][j];
+			}
 		}
 	}
+}
+
+fn scroll(pixels: &mut Vec<Vec<u8>>, todraw: Vec<Vec<u8>>, progress: f32) {
+	let tsize = crossterm::terminal::size().unwrap();
+	let x = (1.0-progress) * (0.25 * tsize.0 as f32 + todraw[0].len() as f32 / 2.0) + progress * (tsize.0 as f32 * 0.75 - todraw[0].len() as f32 / 2.0);
+	
+	display(pixels, todraw, x as i32, tsize.1 as i32 / 2);
 }
 
 fn print_usage(program: &str, opts: Options) {
@@ -82,7 +90,8 @@ fn main() {
 	let program = args[0].clone();
 	
 	let mut opts = Options::new();
-	opts.optopt("f", "font", "set big font", "PATH");
+	opts.optopt("f", "font", "set font", "PATH");
+	opts.optopt("s", "speed", "set text speed (default: 10)", "INTEGER");
 	opts.optflag("l", "loop", "loop text");
 	opts.optflag("h", "help", "print this help menu");
 	let matches = match opts.parse(&args[1..]) {
@@ -97,7 +106,11 @@ fn main() {
 	let do_loop = matches.opt_present("l");
 	let font = match matches.opt_str("f") {
 		Some(path) => Font::try_from_vec(std::fs::read(&path).expect("Error reading font")).expect("Error reading font"),
-		None => Font::try_from_bytes(include_bytes!("../fonts/Montserrat/Montserrat-Black.ttf")as &[u8]).expect("Error reading default font")
+		None => Font::try_from_bytes(include_bytes!("../fonts/Montserrat/Montserrat-Black.ttf") as &[u8]).expect("Error reading default font")
+	};
+	let speed = match matches.opt_str("s") {
+		Some(inverse_speed) => 10.0 / inverse_speed.parse::<i32>().unwrap_or(10) as f32,
+		None => 1.0
 	};
 	let text = if !matches.free.is_empty() {
 		matches.free[0].clone()
@@ -113,22 +126,37 @@ fn main() {
 	crossterm::execute!(stdout, crossterm::terminal::DisableLineWrap).unwrap();
 	crossterm::execute!(stdout, crossterm::terminal::EnterAlternateScreen).unwrap();
 	
-	loop {
+	'outer: loop {
 		for word in text.split_whitespace() { // Main loop
-			let time: i32 =  300 + 60 * word.len() as i32; // Time to show current word for in milliseconds
+			let time = ((200.0 + 60.0 * word.len() as f32 + if PUNCTUATION.contains(word.chars().last().unwrap()) {350.0} else {0.0}) * speed) as i32; // Time to show current word for in milliseconds
 			let timer = Instant::now();
 			while timer.elapsed().as_millis() < time as u128 {
 				let progress = timer.elapsed().as_millis() as f32 / time as f32;
 				let tsize = crossterm::terminal::size().unwrap();
+				let mut pixels: Vec<Vec<u8>> = vec![vec![b' '; tsize.0 as usize]; tsize.1 as usize];
 				
-				let render = render_word(word, &font, (tsize.1 as f32)/3.0, " .:-=+*#%@"); 
-				//crossterm::execute!(stdout, crossterm::terminal::Clear(crossterm::terminal::ClearType::All)).unwrap();
+				let render = render_word(word, &font, ((tsize.1 as f32)/2.0).max(10.0 as f32), " .:-=+*#%@"); 
 				
 				if render[0].len() as u16 > tsize.0 {
-						
+					scroll(&mut pixels, render, progress);    
 				} else {
-					display(render, tsize.0 / 2, tsize.1 / 2);
+					display(&mut pixels, render, tsize.0 as i32 / 2, tsize.1 as i32 / 2);
 				}
+				
+				for i in 0..pixels.len() {
+					crossterm::execute!(stdout, crossterm::cursor::MoveTo(0, i as u16)).unwrap();
+					let mut line = String::new();
+					for j in 0..pixels[i].len() {
+						line.push(pixels[i][j] as char);
+					}
+					print!("{}", line);
+				}
+				
+				if crossterm::event::poll(std::time::Duration::from_secs(0)).unwrap() {
+					if let crossterm::event::Event::Key(_key_event) = crossterm::event::read().unwrap() {
+						break 'outer;
+					}
+				}    
 			}
 		}
 		
@@ -137,6 +165,7 @@ fn main() {
 		}
 	}
 	
+	crossterm::execute!(stdout, crossterm::terminal::EnableLineWrap).unwrap();
 	crossterm::execute!(stdout, crossterm::terminal::LeaveAlternateScreen).unwrap();
 	crossterm::execute!(stdout, crossterm::cursor::Show).unwrap();
 }
